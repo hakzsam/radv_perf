cd349d0439c843789bab8a7164111e58a1a0ee91
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2018-12-11 11:15:58 +0100

    update
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>

--- f12017/f12017.sh
+++ f12017/f12017.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-./f12017.py --iterations=1 --resolution=1920x1080 --preset=ultralow --antialiasing=taa
-./f12017.py --iterations=1 --resolution=1920x1080 --preset=low --antialiasing=taa
-./f12017.py --iterations=1 --resolution=1920x1080 --preset=medium --antialiasing=taa
-./f12017.py --iterations=1 --resolution=1920x1080 --preset=high --antialiasing=taa
-./f12017.py --iterations=1 --resolution=1920x1080 --preset=ultrahigh --antialiasing=taa
+./f12017.py --iterations=3 --resolution=1920x1080 --preset=ultralow --antialiasing=taa
+./f12017.py --iterations=3 --resolution=1920x1080 --preset=low --antialiasing=taa
+./f12017.py --iterations=3 --resolution=1920x1080 --preset=medium --antialiasing=taa
+./f12017.py --iterations=3 --resolution=1920x1080 --preset=high --antialiasing=taa
+./f12017.py --iterations=3 --resolution=1920x1080 --preset=ultrahigh --antialiasing=taa
 
-./f12017.py --iterations=1 --resolution=2560x1440 --preset=ultralow --antialiasing=taa
-./f12017.py --iterations=1 --resolution=2560x1440 --preset=low --antialiasing=taa
-./f12017.py --iterations=1 --resolution=2560x1440 --preset=medium --antialiasing=taa
-./f12017.py --iterations=1 --resolution=2560x1440 --preset=high --antialiasing=taa
-./f12017.py --iterations=1 --resolution=2560x1440 --preset=ultrahigh --antialiasing=taa
+./f12017.py --iterations=3 --resolution=2560x1440 --preset=ultralow --antialiasing=taa
+./f12017.py --iterations=3 --resolution=2560x1440 --preset=low --antialiasing=taa
+./f12017.py --iterations=3 --resolution=2560x1440 --preset=medium --antialiasing=taa
+./f12017.py --iterations=3 --resolution=2560x1440 --preset=high --antialiasing=taa
+./f12017.py --iterations=3 --resolution=2560x1440 --preset=ultrahigh --antialiasing=taa
 
-./f12017.py --iterations=1 --resolution=3840x2160 --preset=ultralow --antialiasing=taa
-./f12017.py --iterations=1 --resolution=3840x2160 --preset=low --antialiasing=taa
-./f12017.py --iterations=1 --resolution=3840x2160 --preset=medium --antialiasing=taa
-./f12017.py --iterations=1 --resolution=3840x2160 --preset=high --antialiasing=taa
-./f12017.py --iterations=1 --resolution=3840x2160 --preset=ultrahigh --antialiasing=taa
+./f12017.py --iterations=3 --resolution=3840x2160 --preset=ultralow --antialiasing=taa
+./f12017.py --iterations=3 --resolution=3840x2160 --preset=low --antialiasing=taa
+./f12017.py --iterations=3 --resolution=3840x2160 --preset=medium --antialiasing=taa
+./f12017.py --iterations=3 --resolution=3840x2160 --preset=high --antialiasing=taa
+./f12017.py --iterations=3 --resolution=3840x2160 --preset=ultrahigh --antialiasing=taa
--- tw2/tw2.sh
+++ tw2/tw2.sh
@@ -3,12 +3,20 @@
 # Estimated time: 17min
 
 # 4k
+./tw2.py --iterations=3 --resolution=3840x2160 --preset=low --scene=fallen
+./tw2.py --iterations=3 --resolution=3840x2160 --preset=low --scene=skaven
+./tw2.py --iterations=3 --resolution=3840x2160 --preset=medium --scene=fallen
+./tw2.py --iterations=3 --resolution=3840x2160 --preset=medium --scene=skaven
 ./tw2.py --iterations=3 --resolution=3840x2160 --preset=ultra --scene=fallen
 ./tw2.py --iterations=3 --resolution=3840x2160 --preset=ultra --scene=skaven
 ./tw2.py --iterations=3 --resolution=3840x2160 --preset=high --scene=fallen
 ./tw2.py --iterations=3 --resolution=3840x2160 --preset=high --scene=skaven
 
 # 1080p
+./tw2.py --iterations=3 --resolution=1920x1080 --preset=low --scene=fallen
+./tw2.py --iterations=3 --resolution=1920x1080 --preset=low --scene=skaven
+./tw2.py --iterations=3 --resolution=1920x1080 --preset=medium --scene=fallen
+./tw2.py --iterations=3 --resolution=1920x1080 --preset=medium --scene=skaven
 ./tw2.py --iterations=3 --resolution=1920x1080 --preset=ultra --scene=fallen
 ./tw2.py --iterations=3 --resolution=1920x1080 --preset=ultra --scene=skaven
 ./tw2.py --iterations=3 --resolution=1920x1080 --preset=high --scene=fallen
