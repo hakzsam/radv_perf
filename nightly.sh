5488fc4a1e1aa0293167b54a22e75f4f7acc8f8a
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2019-02-06 12:10:08 +0100

    add sam2017 to nighlty benchs

--- nightly.sh
+++ nightly.sh
@@ -4,6 +4,7 @@ cd dow3 && ./dow3.sh && cd ..
 cd dota2 && ./dota2.sh && cd ..
 cd f12017 && ./f12017.sh && cd ..
 cd talos && ./talos.sh && cd ..
+cd sam2017 && ./sam2017.sh && cd ..
 cd tob && ./tob.sh && cd ..
 cd tw2 && ./tw2.sh && cd ..
 cd vkmark && ./vkmark.sh && cd ..
---
+++ sam2017/sam2017.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./sam2017.py --resolution=1920x1080
+./sam2017.py --resolution=3840x2160
