307df14c029094ea7debbf1679f5cee5c3df57be
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2019-02-06 11:24:23 +0100

    update rotr.sh script

--- rotr/rotr.sh
+++ rotr/rotr.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-./rotr.py --iterations=1 --resolution=2560x1440 --preset=Medium
-./rotr.py --iterations=1 --resolution=2560x1440 --preset=High
-./rotr.py --iterations=1 --resolution=2560x1440 --preset=VeryHigh
+./rotr.py --iterations=3 --resolution=1920x1080 --preset=Medium
+./rotr.py --iterations=3 --resolution=1920x1080 --preset=High
+./rotr.py --iterations=3 --resolution=1920x1080 --preset=VeryHigh
 
-./rotr.py --iterations=1 --resolution=3840x2160 --preset=Medium
-./rotr.py --iterations=1 --resolution=3840x2160 --preset=High
-./rotr.py --iterations=1 --resolution=3840x2160 --preset=VeryHigh
-
+./rotr.py --iterations=3 --resolution=3840x2160 --preset=Medium
+./rotr.py --iterations=3 --resolution=3840x2160 --preset=High
+./rotr.py --iterations=3 --resolution=3840x2160 --preset=VeryHigh
